5a9bcf24839883174d16c146f3294f66116afc28
AroSaneD <mail.junk.3135@gmail.com>
2018-03-04 21:10:15 +0200

    implemented OSX factory

--- src/app/model/factories/SimpleFactory.ts
+++ src/app/model/factories/SimpleFactory.ts
@@ -1,5 +1,20 @@
 import { AFactory } from "./AFactory";
 
+import { IButton } from './../IButton';
+import { ISlider } from './../ISlider';
+
+import { OSXButton } from "../families/1-osx/OSXButton";
+import { OSXSlider } from "../families/1-osx/OSXSlider";
+
+
 export class SimpleFactory extends AFactory { // A.K.A. the OSX factory
 
+    getButton(): IButton {
+        return new OSXButton();
+    }
+
+    getSlider(): ISlider {
+        return new OSXSlider();
+    }
+    
 }
