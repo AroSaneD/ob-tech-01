8c2fdc1d65db28dffc6a342296c1309ae94577e0
AroSaneD <mail.junk.3135@gmail.com>
2018-03-05 18:18:55 +0200

    added index file

---
+++ src/app/model/index.ts
@@ -0,0 +1,9 @@
+import { AFactory } from "./factories/AFactory";
+import { FactoryType } from './factories/FactoryType';
+
+var factoryType = FactoryType.Simple;
+
+var factory = AFactory.getFactory(factoryType);
+
+var btn = factory.getButton(); // no arguments - osx btn
+var slider = factory.getSlider("red");
